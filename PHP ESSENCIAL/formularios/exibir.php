990419caf79fec7b6946acb3fb32c0e2c03247fb
Ms7Biel <gabriel.msantos7@hotmail.com>
2024-06-03 18:20:24 -0300

    .

---
+++ PHP ESSENCIAL/formularios/exibir.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "O Nome é: " . $_POST['nome'] . "<br>";
+echo "A Idade é: " . $_POST['idade'] . "<br>";
+echo "O E-Mail é: " . $_POST['email'] . "<br>";
---
+++ PHP ESSENCIAL/formularios/noticia.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Noticia</title>
+</head>
+
+<body>
+    <form action="exibir.php" method="post">
+        <label for="nome">Nome: </label>
+        <input type="text" name="nome" id="nome">
+        <br>
+        <br>
+        <label for=" nome">Idade: </label>
+        <input type="number" name="idade" id="idade">
+        <br>
+        <br>
+        <label for="nome">E-mail: </label>
+        <input type="text" name="email" id="Email">
+        <br>
+        <br>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+
+</html>
