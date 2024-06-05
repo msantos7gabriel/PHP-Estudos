fdf36cba7a28259bdd19681f9c5c8fc968cfd900
Ms7Biel <gabriel.msantos7@hotmail.com>
2024-06-05 12:57:50 -0300

    Update index.php

--- PHP ESSENCIAL/formularios/index.php
+++ PHP ESSENCIAL/formularios/index.php
@@ -36,17 +36,28 @@
     </form>
 
     <?php
+    var_dump($_POST);
     echo " <h1>Dados Enviados:</h1>";
     if (isset($_POST['enviado'])) {
         if (empty($_POST['nome']) || strlen($_POST['nome'] < 3 || strlen($_POST['nome'] < 100) || is_numeric($_POST['nome']))) {
             echo "<p class=\"error\">Preencha o campo nome</p>";
-        } elseif (empty($_POST['email'])) {
+        } elseif (empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
             echo "<p class=\"error\">Preencha o campo Email</p>";
+        } elseif (!empty($_POST['website']) && !filter_var($_POST['website'], FILTER_VALIDATE_URL)) {
+            echo "<p class=\"error\">Preencha Corretamente o Campo WebSite</p>";
         } else {
             echo "<p><b>Nome: </b>" . $_POST['nome'] . "</p>";
             echo "<p><b>Email: </b>" . $_POST['email'] . "</p>";
-            echo "<p><b>Comentário: </b>" . $_POST['website'] . "</p>";
-            echo "<p><b>Gênero: </b>" . $_POST['genero'] . "</p>";
+            echo "<p><b>Website: </b>" . $_POST['website'] . "</p>";
+            $cometário = empty($_POST['cometário']) ? '' : $_POST['cometário'];
+            echo "<p><b>Comentário: </b>" . $cometário . "</p>";
+            $genero = "Não Informado";
+            if (isset($_POST["genero"])) {
+                if ($_POST['genero'] != "Masculino" || $_POST['genero'] != "Feminino" || $_POST['genero'] != "Outro") {
+                    $genero = $_POST['genero'];
+                }
+            }
+            echo "<p><b>Gênero: </b>" . $genero . "</p>";
         }
     }
     ?>
